6818dfc7ef81bdc960291a5f5931a32fd84c99a0
Edward Rees <erees.hk@gmail.com>
2023-08-30 09:57:50 -0700

    [feat] setup of experience

--- src/components/experience/index.tsx
+++ src/components/experience/index.tsx
@@ -1,6 +1,17 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserTie,faChalkboardTeacher, faLaptopCode, faPeopleCarry } from "@fortawesome/free-solid-svg-icons";
-import { Dialog, DialogTrigger, DialogContent, DialogTitle, ScrollArea } from "@/components/ui";
+import {
+  faUserTie,
+  faChalkboardTeacher,
+  faLaptopCode,
+  faPeopleCarry,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+  ScrollArea,
+} from "@/components/ui";
 
 type ExperienceProps = {
   title: string;
@@ -12,10 +23,51 @@ type ExperienceProps = {
   link: string;
 
   partTime?: boolean;
-}
+};
 
-const Experience = ({ title, job, period, location, type, duties, link, partTime }: ExperienceProps ) => {
-  return <></>;
-}
+const Experience = ({
+  title,
+  job,
+  period,
+  location,
+  type,
+  duties,
+  link,
+  partTime,
+}: ExperienceProps) => {
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <div className="flex flex-col items-center justify-center text-center">
+          <div className="w-12 h-full">
+            <FontAwesomeIcon
+              icon={
+                type === "education"
+                  ? faChalkboardTeacher
+                  : type === "work"
+                  ? faUserTie
+                  : type === "project"
+                  ? faLaptopCode
+                  : faPeopleCarry
+              }
+              size="3x"
+            />
+          </div>
+          <p className="text-2xl">{title}</p>
+          <p className="text-xl text-center">{job}</p>
+        </div>
+      </DialogTrigger>
+      <DialogContent>
+        <ScrollArea className="h-full w-full">
+          <DialogTitle>
+            <p className="text-2xl">
+              {title} - {job}
+            </p>
+          </DialogTitle>
+        </ScrollArea>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 export { Experience };
