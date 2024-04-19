9965df0bf98dc94d0cbba92bc057433a3883434e
Edward Rees <erees.hk@gmail.com>
2024-04-18 19:39:35 -0700

    [fix] fixed build crash

--- src/app/api/journey/route.ts
+++ src/app/api/journey/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import data from './data.json';
 
-export async function GET (req: NextRequest): NextResponse { 
+export async function GET (req: NextRequest) { 
   return NextResponse.json(data);
  //  return NextResponse.json("Hello World");
 }
